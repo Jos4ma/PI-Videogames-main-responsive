211902f49eb673cbb4136d6cdeb4d0fb62a12556
Jos4ma <88600626+Jos4ma@users.noreply.github.com>
2023-08-02 22:57:00 -0400

    update detail resposnive

--- client/src/components/Detail.jsx
+++ client/src/components/Detail.jsx
@@ -34,32 +34,32 @@ console.log(videogameDetail.genders)
         {videogameDetail.id ? ( 
             <div className={style.detail}>
                 <div className={style.title}><h2 className={style.h2}>Videogame name: {videogameDetail.name} </h2></div>
-                <div className={style.first}>
-                  <div className={style.boxPicture}>
-                    <img className={style.img} src={videogameDetail.image} alt="Not Found" />        
-                  </div>
-                  <div className={style.boxInfo}>
-                  <div className={style.order}>    
-                    <h5 className={style.h5}>ID: {videogameDetail.id} </h5>        
-                    <h5 className={style.h5}>RATING: {videogameDetail.rating} </h5>
-                    <h5 className={style.h5}>RELEASED: {videogameDetail.released} </h5>
-                    {/* {videogameDetail.platforms?.map((platform,i)=>(
-                      <h5 key={i} className={style.h5} >{platform}</h5> 
-                    ))}  */}
-                    <h5 className={style.h5} >PLATFORMS RELATED: {videogameDetail.platforms}</h5>
-                    <h5 className={style.h5}>GENRES: {videogameDetail.genders} </h5> 
-                  </div>
-                    <Link to={"/home"}>
+                  <div className={style.first}>
+                    <div className={style.boxPicture}>
+                      <img className={style.img} src={videogameDetail.image} alt="Not Found" />        
+                    </div>
+                    <div className={style.boxInfo}>
+                      <div className={style.order}>    
+                        <h5 className={style.h5}>ID: {videogameDetail.id} </h5>        
+                        <h5 className={style.h5}>RATING: {videogameDetail.rating} </h5>
+                        <h5 className={style.h5}>RELEASED: {videogameDetail.released} </h5>
+                        {/* {videogameDetail.platforms?.map((platform,i)=>(
+                         <h5 key={i} className={style.h5} >{platform}</h5> 
+                        ))}  */}
+                        <h5 className={style.h5} >PLATFORMS RELATED: {videogameDetail.platforms}</h5>
+                        <h5 className={style.h5}>GENRES: {videogameDetail.genders} </h5> 
+                      </div>
+                      <Link to={"/home"}>
                         <button className={style.button}>HOME</button>
-                    </Link>
-                    {/* <Link to={"/home"}>
+                      </Link>
+                      {/* <Link to={"/home"}>
                         <button className={style.button}>DELETE POKEMON</button>
-                    </Link> */}
-                    {videogameDetail.id.length > 5 ? (
-                    <button className={style.button}  onClick={(e) => handleDelete(e)}>
-                      DELETE THIS VIDEOGAME
-                    </button>
-                    ) : null}
+                      </Link> */}
+                      {videogameDetail.id.length > 5 ? (
+                      <button className={style.button}  onClick={(e) => handleDelete(e)}>
+                         DELETE THIS VIDEOGAME
+                      </button>
+                      ) : null}
                     </div>
                   </div>
                 
