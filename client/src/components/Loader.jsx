285b36e1f46d7c2ece12522ec79e8c1415ddfdb6
Jos4ma <88600626+Jos4ma@users.noreply.github.com>
2023-08-04 11:10:40 -0400

    fix picture loader

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import style from "./Loader.module.css";
 
 export default function Loader() {
-  return <div className={style.spinner}>LOADING!</div>;
+  return <div className={style.charge}><div className={style.spinner}>LOADING!</div></div>;
 }
