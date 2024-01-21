8eed31ca894a51a5b0b0dd84ad23bdfa763d1c44
Jos4ma <88600626+Jos4ma@users.noreply.github.com>
2024-01-21 11:24:05 -0300

    new update for React in store/index.js  with configureStore

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,29 @@
-import { applyMiddleware, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk'
-import rootReducer from '../reducer/index'
+import { configureStore } from '@reduxjs/toolkit'
 
-const store = createStore (
+import rootReducer from '../reducer/index'
+//import usersReducer from '../reducers/usersReducer'
+
+// Automatically adds the thunk middleware and the Redux DevTools extension
+const store = configureStore({
+  // Automatically calls `combineReducers`
+  reducer: {
     rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
-)
+    //users: usersReducer
+  }
+})
 
 export default store
+
+// import { applyMiddleware, createStore } from 'redux'
+// import { composeWithDevTools } from 'redux-devtools-extension';
+// import thunk from 'redux-thunk'
+// import rootReducer from '../reducers/index'
+
+
+// const store = createStore (
+//     rootReducer,
+//     composeWithDevTools(applyMiddleware(thunk))
+// )
+
+
+// export default store
